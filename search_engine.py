93acef6f1da0eeb6a46a03e62f9b8753d70615a2
daljeetkaurJohar <daljeetkaur07@gmail.com>
2026-02-20 14:21:21 +0530

    Delete search_engine.py

--- search_engine.py
+++
@@ -1,8 +0,0 @@
-
-def build_full_context(row):
-    return " | ".join([f"{col}: {row[col]}" for col in row.index])
-
-def search_rows(df, query):
-    query = query.lower()
-    mask = df.apply(lambda r: query in build_full_context(r).lower(), axis=1)
-    return df[mask]
